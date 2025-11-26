59ef0a9bd80511a26593247a272d00c4e845dad2
Andrei <ascurtuny@gmail.com>
2025-11-26 11:44:27 +0200

    fixed stupid mistake

--- lua/plugins/vague.lua
+++ lua/plugins/vague.lua
@@ -1,12 +1,9 @@
 return {
-    "vague2k/vague.nvim",
-    config = function() 
-        require("vague").setup({
-<<<<<<< HEAD
-          transparent = true,
-=======
-            -- no setup at the moment
->>>>>>> c619f456e353be56b07bbb774e3b5f5a85f7619f
-        })
-    end,
+  "vague2k/vague.nvim",
+  config = function()
+    require("vague").setup({
+      transparent = true,
+      -- no setup at the moment
+    })
+  end,
 }
